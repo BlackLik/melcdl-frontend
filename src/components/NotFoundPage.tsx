6a1d4d0219f1fa054741afa2f556d8fa8e640880
as.shiryeav <alssval@gmail.com>
2025-05-31 22:09:16 +0300

    feat: add normal path

---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="text-3xl font-bold underline">404</div>;
+};
+
+export default Home;
--- src/router.ts
+++ src/router.ts
@@ -2,15 +2,25 @@ import { createBrowserRouter } from 'react-router';
 import { lazy } from 'react';
 import LoaderWrapper from '@/components/LoaderWrapper';
 
-const Home = lazy(() => import('@/components/Home'));
+const HomePage = lazy(() => import('@/components/HomePage'));
+const NotFoundPage = lazy(() => import("@/components/NotFoundPage"))
 
 
 export const router = createBrowserRouter(
   [
     {
-      path: '/',
+      path: "/",
       Component: LoaderWrapper,
-      children: [{ index: true, Component: Home }],
+      children: [
+        { index: true, Component: HomePage },
+        { path: 'login/', Component: HomePage},
+        { path: 'registration/', Component: HomePage},
+        { path: 'results/', children: [
+            {index: true, Component: HomePage},
+            {path: ':id/',Component: HomePage}
+        ]},
+        { path: '*', Component: NotFoundPage},
+      ],
     },
   ],
   {
